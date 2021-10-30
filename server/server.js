8304a8422e3637d49f3f1ee8b935659a4dc86b79
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-10-30 10:22:24 -0400

    Working on server: app.get function + GraphQl link with PORT

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('../connection');
 const routes = require('./routes');
 const {ApolloServer} = require('apollo-server-express');
-const { typeDefs, resolvers } = require
+const { typeDefs, resolvers } = require ('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,8 +23,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//builds out provided index.html front end code
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
