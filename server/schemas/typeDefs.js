dab38347aed7b71756980af8d241e57d15b7c361
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-11-01 15:18:54 -0400

    Syntax clean up on Schema + mutation initials

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,21 +3,34 @@ const {gql} = require('apollo-server-express');
 const typeDefs = gql`
 type User {
     _id: ID
-    username: string!
-    Email: string!
+    username: String!
+    Email: String!
     password: String!
-    Savedbooks: [Book]
+    savedBooks: [Book]
 }
 
 type Book {
-    boodid: string!
-    authors: string!
-    description: string
-    image: string
-    link: string
+    boodId: String!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!
 }
 
 type AUTH {
     user: User
     toked: ID!
 }
+
+type Query {
+    me: User
+}
+
+type mutation {
+    login(email: String!, password: String!): Auth
+    newUser(username: String!, email: Sgring! password: Sttring!): Auth
+    storeBook(bookID: String!, title: String! authors: String! description: String! image: String! link: String!)
+    deleteBook(bookId: String!)
+}
+
