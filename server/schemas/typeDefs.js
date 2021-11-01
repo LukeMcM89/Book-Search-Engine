81abe8902ded965afe78c396ccda91ab9f05345b
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-11-01 15:11:03 -0400

    Schema TypeDef additions

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,23 @@
 const {gql} = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: string!
+    Email: string!
+    password: String!
+    Savedbooks: [Book]
+}
+
+type Book {
+    boodid: string!
+    authors: string!
+    description: string
+    image: string
+    link: string
+}
+
+type AUTH {
+    user: User
+    toked: ID!
+}
